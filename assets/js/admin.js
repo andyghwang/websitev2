05325c91c99e2c2d5290ad4c5c357a9db60a0e37
Andy Hwang <andy.g.hwang@gmail.com>
2024-04-20 21:09:56 -0700

    posts page and table created

---
+++ assets/js/admin.js
@@ -0,0 +1,23 @@
+// Sidebar Responsiveness
+
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const navOverlay = document.querySelector('.nav-overlay');
+const dropdowns = document.querySelectorAll('.navitem .dropdown')
+
+menuIcon.addEventListener('click', function(){
+    sidebar.classList.add('open');
+    navOverlay.classList.add('open');
+})
+
+navOverlay.addEventListener('click', function(){
+    navOverlay.classList.remove('open');
+    sidebar.classList.remove('open');
+})
+
+dropdowns.forEach(dropdown => {
+    const navItem = dropdown.closest('.navitem');
+    navItem.addEventListener('click', function(){
+        navItem.classList.toggle('active');
+    });
+})
