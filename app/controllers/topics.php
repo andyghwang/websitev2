d4dbecf1331c91843509267e0c265aca48021dbf
Andy Hwang <andy.g.hwang@gmail.com>
2024-05-12 23:12:04 -0700

    fixed another edit topic bug where error constantly showed when changing description

--- app/controllers/topics.php
+++ app/controllers/topics.php
@@ -8,25 +8,24 @@ $table = "topics";
 $topics = selectAll($table);
 
 // Topic Validation
-function validateTopic($topic) {  
-    global $errors;
-    global $table;
-    
-    $existingTopic = selectOne($table, ['name' => $topic['name']]);
+function validateTopic($topic) {
+    global $table;  
+    $errors = array();
+     
     if (empty($topic['name'])) {
         array_push($errors, 'Please enter a topic.');
-    } elseif ($existingTopic['name'] == $topic['name']){
-        array_push($errors, 'This topic already exists.');
     } 
-    return $errors;
-}
 
-function validateUpdate($topic) {
-    global $errors;
-
-    if (empty($topic['name'])) {
-        array_push($errors, 'Please enter a topic name.');
-    }
+    $existingTopic = selectOne($table, ['name' => $topic['name']]);
+    if ($existingTopic){
+        
+        if (isset($topic['update-topic-btn']) && $topic['id'] != $existingTopic['id']) {
+            array_push($errors, 'This topic already exists.');
+        }
+        if (isset($topic['create-topic-btn'])) {
+            array_push($errors, 'This topic already exists.');
+        }        
+    } 
     return $errors;
 }
 
@@ -37,7 +36,7 @@ if (isset($_POST['create-topic-btn'])) {
 
     if (empty($errors)) {
         unset($_POST['create-topic-btn']);
-        create($table, $_POST);
+        $topic_id = create($table, $_POST);
         $_SESSION['message'] = "Topic created successfully.";
         $_SESSION['type'] = "success";
         // Redirect
@@ -51,7 +50,6 @@ if (isset($_POST['create-topic-btn'])) {
 
 // Edit Topic
 if (isset($_GET['id'])) {
-
     $id = $_GET['id'];
     $topic = selectOne($table, ['id' => $id]);
     $id = $topic['id'];
@@ -60,20 +58,23 @@ if (isset($_GET['id'])) {
 }
 
 if (isset($_POST['update-topic-btn'])) {
-
-    $errors = validateUpdate($_POST);
+    $errors = validateTopic($_POST);
     $id = $_POST['id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
     if (empty($errors)) {       
         unset($_POST['update-topic-btn']);
         unset($_POST['id']);
-        update($table, $id, $_POST);
+        $topic_id = update($table, $id, $_POST);
         $_SESSION['message'] = "Topic updated successfully.";
         $_SESSION['type'] = "success";
         // Redirect
         header('location: ' . BASE_URL . '/admin/topics/index.php');
         exit();  
+    } else {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $description = $_POST['description'];
     }
 }
 
